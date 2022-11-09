ae5442c96aea09ab51d727aea764c1f6a1dafefd
raSel <mdraselm413@gmail.com>
2022-11-09 20:53:23 +0600

    loader

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -5,8 +5,17 @@ import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 
+
+/* 
+1. count,
+2. per page,
+3. page number,
+4. 
+
+*/
+
 const Shop = () => {
-    const products = useLoaderData();
+    const {products} = useLoaderData();
     const [cart, setCart] = useState([]);
 
     const clearCart = () =>{
@@ -18,7 +27,7 @@ const Shop = () => {
         const storedCart = getStoredCart();
         const savedCart = [];
         for(const id in storedCart){
-            const addedProduct = products.find(product => product.id === id);
+            const addedProduct = products.find(product => product._id === id);
             if(addedProduct){
                 const quantity = storedCart[id];
                 addedProduct.quantity = quantity;
@@ -31,19 +40,19 @@ const Shop = () => {
     const handleAddToCart = (selectedProduct) =>{
         console.log(selectedProduct);
         let newCart = [];
-        const exists = cart.find(product => product.id === selectedProduct.id);
+        const exists = cart.find(product => product._id === selectedProduct._id);
         if(!exists){
             selectedProduct.quantity = 1;
             newCart = [...cart, selectedProduct];
         }
         else{
-            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            const rest = cart.filter(product => product._id !== selectedProduct._id);
             exists.quantity = exists.quantity + 1;
             newCart = [...rest, exists];
         }
         
         setCart(newCart);
-        addToDb(selectedProduct.id);
+        addToDb(selectedProduct._id);
     }
 
     return (
@@ -51,7 +60,7 @@ const Shop = () => {
             <div className="products-container">
                 {
                     products.map(product=><Product 
-                        key={product.id}
+                        key={product._id}
                         product={product}
                         handleAddToCart={handleAddToCart}
                         ></Product>)
